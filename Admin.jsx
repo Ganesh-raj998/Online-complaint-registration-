98f51188605fd5426e299e8ef74cd84cb9773c4a
Ganesh-raj998 <ganiraj2007@gmail.com>
2026-07-20 15:51:16 +0530

    Update server/Admin.jsx

--- Admin.jsx
+++ Admin.jsx
@@ -4,7 +4,7 @@ import Officer from './Officer';
 const Admin = ({ user }) => {
   return (
     <div style={{border: '10px solid #004a99', minHeight: '100vh'}}>
-      <div className="navbar" style={{background: '#333'}}>
+      <div className="navbar" style={{background: '#333'}}>b
         <h1>ADMIN DASHBOARD</h1>
       </div>
       <Officer user={user} isAdminView={true} />
